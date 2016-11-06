18bdd220b8f5fe7bd3291d4af7d62553d851e54b
dbwjd/201611063_j <dbwjd@users.noreply.github.com>
2016-11-07 01:52:19 +0900

    구구단

---
+++ Calculator.java
@@ -0,0 +1,16 @@
+class Calculator{ 
+	int[] x = {1, 2, 3, 4, 5, 6, 7, 8, 9}; 
+	 
+	public void printCalculator() { 
+		for (int i: x) { 
+			System.out.printf("\n%d¢¥U -", i); 
+			for (int j: x) 
+				System.out.printf("\t%dx%d=%2d ", i, j, i * j); 
+ 	  } 
+	} 
+	 
+	public static void main(String[] args) { 
+		Calculator gugudan = new Calculator(); 
+		gugudan.printCalculator(); 
+	} 
+}

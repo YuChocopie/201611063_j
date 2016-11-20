9195bb1c20885b702bf3d4eb044aff857839c9a9
dbwjd/201611063_j <dbwjd@users.noreply.github.com>
2016-11-20 22:25:38 +0900

    데이터구조-6: 문자열 'hello' 뒤집기

---
+++ OverTurn.java
@@ -0,0 +1,11 @@
+import java.lang.StringBuffer;
+class OverTurn {
+    static String s="hello";
+    static StringBuffer sb = new StringBuffer(s);
+    public static void main(String[] args) {
+        System.out.print(sb);
+        System.out.print("--");
+        sb.reverse();
+        System.out.print(sb);
+    }
+}

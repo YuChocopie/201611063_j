562d438483c173298aaee4e91a6b1331ce1fbac5
dbwjd/201611063_j <dbwjd@users.noreply.github.com>
2016-12-04 22:37:42 +0900

    문제 oo-4: 클래스 상속을 사용하여 도형의 면적을 구하기

---
+++ Rect.java
@@ -0,0 +1,30 @@
+package com.sd.ch8;
+
+public class Rect extends Shape {
+  private double width;
+  private double height;
+  private double area;
+  private double perimeter;
+  
+  public Rect(double w, double h) {
+    this.width = w;
+    this.height = h;
+    this.area = w*h;
+    this.perimeter = 2*(w+h);
+  }
+  public double calcArea() {
+    return this.area;
+  }
+  public double calcPerimeter() {
+    return this.perimeter;
+  }
+  public String toString() {
+    return "Rect..."+super.toString();
+  }
+  public static void main(String[] args) {
+    Rect r=new Rect(10.0, 20.0);
+    System.out.println("area = "+r.calcArea());
+    System.out.println("perimeter = " + r.calcPerimeter());
+    System.out.print(r.toString());
+  }
+}

98d47326202eb825e6a1e9296a873ae893c29e83
dbwjd/201611063_j <dbwjd@users.noreply.github.com>
2016-12-22 01:20:01 +0900

    Q4 도형면적 미제출과제

--- Shape.java
+++ Shape.java
@@ -1,14 +1,11 @@
 package com.sd.ch8;
 
 public abstract class Shape {
-  public String name="Shape"; 
-  public abstract double calcArea();
-  public abstract double calcPerimeter();
-  public String getName(){
-    return this.name;
-  }
-  public String toString(){
-    return "Shape..."+super.toString();
-  }
-  
+    public String name="Shape";
+    public abstract double calcArea();
+    public abstract double calcPerimeter();
+    public String getName() { return this.name; }
+    public String toString() {
+        return "Shape..."+super.toString();
+    }
 }

868fb48dbb5ce87515a27b6569f0370327a788ee
dbwjd/201611063_j <dbwjd@users.noreply.github.com>
2016-12-22 01:14:00 +0900

    q2거북이상속 미제출과제

--- Turtle.java
+++ Turtle.java
@@ -1,11 +1,6 @@
 package com.sd.ch8;
-public class Turtle {
-    private int nLimbs=4;
-    protected String food;
-    public void setFood(String f) {
-        this.food=f;
-    }
-    public void eat() {
-        System.out.print("Turtle eats "+food);
+class LandTurtle extends Turtle {
+    public void walk() {
+        System.out.print("A land turtle walks...");
     }
 }
